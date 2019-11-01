a0ebf3c14697e7c06fffa608f71853d314003fe1
kiki <hj.chen@bmi-tech.com>
2019-11-01 16:40:17 +0800

    update script

--- for-push2github.sh
+++ for-push2github.sh
@@ -4,7 +4,7 @@ set githubURL [lindex $argv 0]
 set githubUser [lindex $argv 1]
 set githubPWD [lindex $argv 2]
 spawn git remote add github $githubURL
-spawn git push -f github master:master
+spawn git push -f github remotes/origin/master:master
 set timeout 10
 expect "Username for 'https://github.com':"
 send "$githubUser\r"
